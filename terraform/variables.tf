dc3d53bb4589913a05a39128370a8610ac3eb8b9
Marwy999 <announ.marwan@yahoo.it>
2026-09-25 11:40:19 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,20 @@
+# Resource naming prefix
+variable "prefix" {
+  type        = string
+  default     = "secapp"
+  description = "Prefix applied to all created resources."
+}
 
+# Azure target region
+variable "location" {
+  type        = string
+  default     = "westeurope"
+  description = "Primary Azure region for deployment."
+}
+
+# Environment environment name
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "Target deployment environment."
+}
