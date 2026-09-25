5edb0303a14e7185bce226bbea2bd8292f2cb038
Marwy999 <announ.marwan@yahoo.it>
2026-09-25 11:43:45 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,19 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "Target resource group name."
+}
 
+output "web_app_url" {
+  value       = "https://${azurerm_linux_web_app.app.default_hostname}"
+  description = "Public Web App endpoint."
+}
+
+output "key_vault_uri" {
+  value       = azurerm_key_vault.kv.vault_uri
+  description = "Key Vault service URI."
+}
+
+output "storage_account_name" {
+  value       = azurerm_storage_account.sa.name
+  description = "Storage account name."
+}
